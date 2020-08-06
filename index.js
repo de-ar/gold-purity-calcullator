2473a07f53c720dd6bbe21e2314488d34da6d842
Arslan Ahmad <bitf16a018@pucit.edu.pk>
2020-08-06 13:11:54 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+document.getElementById('output').style.visibility = 'hidden';
+document.getElementById('wbd').addEventListener('input', function (e) {
+  document.getElementById('output').style.visibility = 'visible';
+  let wad = document.getElementById('wad').value;
+  let wbd = document.getElementById('wbd').value;
+  document.getElementById('sum').innerHTML = ((wad / wbd) * 1000).toFixed(2);
+  document.getElementById('karat').innerHTML = ((wad / wbd) * 24).toFixed(2);
+  document.getElementById('impurity').innerHTML = wbd - wad;
+  document.getElementById('impurity-per-gram').innerHTML = (
+    (wbd - wad) /
+    wbd
+  ).toFixed(3);
+  document.getElementById('impurity-per-tola').innerHTML = (
+    ((wbd - wad) / wbd) *
+    11.664
+  ).toFixed(3);
+  document.getElementById('impurity-per-ratti').innerHTML = (
+    ((wbd - wad) / wbd) *
+    96
+  ).toFixed(3);
+});
+
+document.getElementById('wad').addEventListener('input', function (e) {
+  document.getElementById('output').style.visibility = 'visible';
+  document.getElementById('output').style.visibility = 'visible';
+  let wad = document.getElementById('wad').value;
+  let wbd = document.getElementById('wbd').value;
+  document.getElementById('sum').innerHTML = ((wad / wbd) * 1000).toFixed(2);
+  document.getElementById('karat').innerHTML = ((wad / wbd) * 24).toFixed(2);
+  document.getElementById('impurity').innerHTML = wbd - wad;
+  document.getElementById('impurity-per-gram').innerHTML = (
+    (wbd - wad) /
+    wbd
+  ).toFixed(3);
+  document.getElementById('impurity-per-tola').innerHTML = (
+    ((wbd - wad) / wbd) *
+    11.664
+  ).toFixed(3);
+  document.getElementById('impurity-per-ratti').innerHTML = (
+    ((wbd - wad) / wbd) *
+    96
+  ).toFixed(3);
+});
