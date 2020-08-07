1f8aeae1cb1622330d0ab42208fc2648016bbf0f
Arslan Ahmad <bitf16a018@pucit.edu.pk>
2020-08-07 09:52:54 +0500

    added button

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
 document.getElementById('output').style.visibility = 'hidden';
-document.getElementById('wbd').addEventListener('input', function (e) {
-  document.getElementById('output').style.visibility = 'visible';
-  let wad = document.getElementById('wad').value;
-  let wbd = document.getElementById('wbd').value;
-  document.getElementById('sum').innerHTML = ((wad / wbd) * 1000).toFixed(2);
-  document.getElementById('karat').innerHTML = ((wad / wbd) * 24).toFixed(2);
-  document.getElementById('impurity').innerHTML = wbd - wad;
-  document.getElementById('impurity-per-gram').innerHTML = (
-    (wbd - wad) /
-    wbd
-  ).toFixed(3);
-  document.getElementById('impurity-per-tola').innerHTML = (
-    ((wbd - wad) / wbd) *
-    11.664
-  ).toFixed(3);
-  document.getElementById('impurity-per-ratti').innerHTML = (
-    ((wbd - wad) / wbd) *
-    96
-  ).toFixed(3);
-});
 
-document.getElementById('wad').addEventListener('input', function (e) {
-  document.getElementById('output').style.visibility = 'visible';
+const handleButtonClick = () => {
   document.getElementById('output').style.visibility = 'visible';
   let wad = document.getElementById('wad').value;
   let wbd = document.getElementById('wbd').value;
@@ -40,4 +19,4 @@ document.getElementById('wad').addEventListener('input', function (e) {
     ((wbd - wad) / wbd) *
     96
   ).toFixed(3);
-});
+};
