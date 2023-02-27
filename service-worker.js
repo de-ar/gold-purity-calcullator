99d5906813962fd474b1f5b2132dd3a01d3db20d
Arslan Ahmad <arslan.ahmad@pm.me>
2023-02-28 00:28:53 +0500

    chore: remove console logs

--- service-worker.js
+++ service-worker.js
@@ -15,10 +15,8 @@ const appShellFiles = [
 ];
 
 self.addEventListener("install", (e) => {
-  console.log("[Service Worker] Install");
   e.waitUntil(
     caches.open(cacheName).then((cache) => {
-      console.log("[Service Worker] Caching all: app shell and content");
       return cache.addAll(appShellFiles);
     })
   );
@@ -28,13 +26,11 @@ self.addEventListener("fetch", (e) => {
   e.respondWith(
     (async () => {
       const r = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
       if (r) {
         return r;
       }
       const response = await fetch(e.request);
       const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
       cache.put(e.request, response.clone());
       return response;
     })()
